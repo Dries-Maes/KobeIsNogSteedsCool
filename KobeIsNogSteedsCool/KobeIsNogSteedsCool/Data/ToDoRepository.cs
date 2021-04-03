33c44a7c7b2fede10954296275b44cdf65711848
Simon Van Tittelboom <simon.van.tittelboom@gmail.com>
2021-04-03 17:19:53 +0200

    Add Save Todo

---
+++ KobeIsNogSteedsCool/KobeIsNogSteedsCool/Data/ToDoRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+using KobeIsNogSteedsCool.Models;
+
+namespace KobeIsNogSteedsCool.Data
+    {
+    internal class ToDoRepository
+        {
+        public ToDo GetToDo(int id)
+            {
+            using (var dbContext = new KobeIsNogSteedsCoolContext())
+                {
+                return dbContext.ToDos.FirstOrDefault(x => x.Id == id);
+                }
+            }
+
+        public void SaveToDo(ToDo todo)
+            {
+            using (var dbContext = new KobeIsNogSteedsCoolContext())
+                {
+                if (todo.Id == 0)
+                    dbContext.ToDos.Add(todo);
+                else
+                    dbContext.ToDos.Update(todo);
+
+                dbContext.SaveChanges();
+                }
+            }
+        }
+    }
--- KobeIsNogSteedsCool/KobeIsNogSteedsCool/KobeIsNogSteedsCoolContext.cs
+++ KobeIsNogSteedsCool/KobeIsNogSteedsCool/KobeIsNogSteedsCoolContext.cs
@@ -1,27 +1,30 @@
 ﻿using KobeIsNogSteedsCool.Models;
+
 using Microsoft.EntityFrameworkCore;
+
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+
 using Xamarin.Essentials;
 
 namespace KobeIsNogSteedsCool
-{
-    internal class KobeIsNogSteedsCoolContext : DbContext
     {
+    internal class KobeIsNogSteedsCoolContext : DbContext
+        {
         public DbSet<Note> Notes { get; set; }
-        public DbSet<ToDo> ToDo { get; set; }
+        public DbSet<ToDo> ToDos { get; set; }
 
         public KobeIsNogSteedsCoolContext()
-        {
+            {
             Database.EnsureCreated();
-        }
+            }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
+            {
             string dbPath = Path.Combine(FileSystem.AppDataDirectory, "HowYouDoin.Ward");
             optionsBuilder.UseSqlite($"Filename={dbPath}");
+            }
         }
     }
-}
